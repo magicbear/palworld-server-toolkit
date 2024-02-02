a028c653aef0a722076183c562d9766bcec5cc87
MagicBear <mb@bilibili.com>
2024-02-03 05:47:54 +0800

    Update: list.py using new version of palworld-save-tools Add: taskset script

--- list.py
+++ list.py
@@ -9,7 +9,6 @@ client = Client('127.0.0.1', 25575, passwd='your password', timeout=0.5)
 client.connect(True)
 # print(client.run('Info'))
 
-uesave_path = "/root/.cargo/bin/uesave"
 player_cache = "/data/Pal-Server/player.txt"
 mapping = {}
 
@@ -37,66 +36,58 @@ try:
 except UnicodeDecodeError as e:
     pass
 
-def prettySaveStruct(data, level = 0):
+def PrettyPrint(data, level = 0):
     simpleType = ['DateTime', 'Guid', 'LinearColor', 'Quat', 'Vector', 'PalContainerId']
-    if "Struct" in data:
-        if 'struct_type' in data['Struct']:
-            if data['Struct']['struct_type'] == 'DateTime':
-                print("%s<Value Type='DateTime'>%d</Value>" % ("  " * level, data['Struct']['value']['DateTime']))
-            elif data['Struct']['struct_type'] == 'Guid':
-                print("\033[96m%s\033[0m" % (data['Struct']['value']['Guid']), end="")
-            elif data['Struct']['struct_type'] == "LinearColor":
-                print("%.f %.f %.f %.f" % (data['Struct']['value']['LinearColor']['r'],
-                                                                               data['Struct']['value']['LinearColor']['g'],
-                                                                               data['Struct']['value']['LinearColor']['b'],
-                                                                               data['Struct']['value']['LinearColor']['a']), end="")
-            elif data['Struct']['struct_type'] == "Quat":
-                print("%.f %.f %.f %.f" % (data['Struct']['value']['Quat']['x'],
-                                                                               data['Struct']['value']['Quat']['y'],
-                                                                               data['Struct']['value']['Quat']['z'],
-                                                                               data['Struct']['value']['Quat']['w']), end="")
-            elif data['Struct']['struct_type'] == "Vector":
-                print("%.f %.f %.f" % (data['Struct']['value']['Vector']['x'],
-                                                                               data['Struct']['value']['Vector']['y'],
-                                                                               data['Struct']['value']['Vector']['z']), end="")
-            elif "Struct" in data['Struct']['struct_type'] and data['Struct']['struct_type']["Struct"] == "PalContainerId":
-                print("\033[36m%s\033[0m" % data['Struct']['value']['Struct']['ID']['Struct']['value']['Guid'], end="")
-            elif isinstance(data['Struct']['struct_type'], dict):
-                print("%s<%s>" % ("  " * level, data['Struct']['struct_type']['Struct']))
-                # prettySaveStruct(data['Struct']['value']['Struct'], level + 1)
-                for key in data['Struct']['value']:
-                    prettySaveStruct(data['Struct']['value'], level + 1)
-                print("%s</%s>" % ("  " * level, data['Struct']['struct_type']['Struct']))
-            else:
-                print("Unknow Type ", data['Struct']['struct_type'])
+    if 'struct_type' in data:
+        if data['struct_type'] == 'DateTime':
+            print("%s<Value Type='DateTime'>%d</Value>" % ("  " * level, data['value']))
+        elif data['struct_type'] == 'Guid':
+            print("\033[96m%s\033[0m" % (data['value']), end="")
+        elif data['struct_type'] == "LinearColor":
+            print("%.f %.f %.f %.f" % (data['value']['r'],
+                                                                           data['value']['g'],
+                                                                           data['value']['b'],
+                                                                           data['value']['a']), end="")
+        elif data['struct_type'] == "Quat":
+            print("%.f %.f %.f %.f" % (data['value']['x'],
+                                                                           data['value']['y'],
+                                                                           data['value']['z'],
+                                                                           data['value']['w']), end="")
+        elif data['struct_type'] == "Vector":
+            print("%.f %.f %.f" % (data['value']['x'],
+                                                                           data['value']['y'],
+                                                                           data['value']['z']), end="")
+        elif data['struct_type'] == "PalContainerId":
+            print("\033[96m%s\033[0m" % (data['value']['ID']['value']), end="")
+        elif isinstance(data['struct_type'], dict):
+            print("%s<%s>" % ("  " * level, data['struct_type']))
+            for key in data['value']:
+                PrettyPrint(data['value'], level + 1)
+            print("%s</%s>" % ("  " * level, data['struct_type']))
         else:
-            for key in data['Struct']:
-                if 'Struct' in data['Struct'][key] and 'struct_type' in data['Struct'][key]['Struct'] and (data['Struct'][key]['Struct']['struct_type'] in simpleType or
-                                                                                                           ("Struct" in data['Struct'][key]['Struct']['struct_type'] and data['Struct'][key]['Struct']['struct_type']['Struct'] in simpleType)):
-                    print("%s<%s type='%s'>" % ("  " * level, key, data['Struct'][key]['Struct']['struct_type']), end="")
-                    prettySaveStruct(data['Struct'][key], level + 1)
-                    print("</%s>" % (key))
-                elif "Int" in data['Struct'][key]:
-                    print("%s<%s Type='int'>\033[95m%d\033[0m</%s>" % ("  " * level, key, data['Struct'][key]['Int']['value'], key))
-                elif "Float" in data['Struct'][key]:
-                    print("%s<%s Type='Float'>\033[95m%f\033[0m</%s>" % ("  " * level, key, data['Struct'][key]['Float']['value'], key))
-                elif "Bool" in data['Struct'][key]:
-                    print("%s<%s Type='Bool'>\033[91m%d\033[0m</%s>" % ("  " * level, key, data['Struct'][key]['Bool']['value'], key))
-                elif "Name" in data['Struct'][key]:
-                    print("%s<%s Type='Name'>\033[93m%s\033[0m</%s>" % ("  " * level, key, data['Struct'][key]['Name']['value'], key))
-                elif "Array" in data['Struct'][key]:
-                    print("%s<%s Type='Array' array_type='%s'>%s</%s>" % ("  " * level, key, data['Struct'][key]['Array']['array_type'],
-                                                                          data['Struct'][key]['Array']['value']['Base']['Name'],
-                                                                          key))
-                # elif "Struct" in data['Struct'][key] and 'struct_type' in data['Struct'][key]['Struct'] and "Struct" in data['Struct'][key]['Struct']['struct_type'] and data['Struct'][key]['Struct']['struct_type']['Struct'] == "PalContainerId":
-                #     print("%s<%s Type='PalContainerId'>\033[36m%s\033[0m</%s>" % ("  " * level, key, data['Struct'][key]['Struct']['value']['Struct']['ID']['Struct']['value']['Guid'], key))
-                else:
-                    print("%s<%s Type='\033[31munknow struct\033[0m'>" % ("  " * level, key))
-                    prettySaveStruct(data['Struct'][key], level + 1)
-                    print("%s</%s>" % ("  " * level, key))
+            PrettyPrint(data['value'], level + 1)
     else:
-
-        print("%s%s" % ("  " * level, data))
+        for key in data:
+            if not isinstance(data[key], dict):
+                print("%s<%s type='unknow'>%s</%s>" % ("  " * level, key, data[key], key))
+                continue
+            if 'struct_type' in data[key] and data[key]['struct_type'] in simpleType:
+                print("%s<%s type='%s'>" % ("  " * level, key, data[key]['struct_type']), end="")
+                PrettyPrint(data[key], level + 1)
+                print("</%s>" % (key))
+            elif 'type' in data[key] and data[key]['type'] in ["IntProperty", "Int64Property", "BoolProperty"]:
+                print("%s<%s Type='%s'>\033[95m%d\033[0m</%s>" % ("  " * level, key, data[key]['type'], data[key]['value'], key))
+            elif 'type' in data[key] and data[key]['type'] == "FloatProperty":
+                print("%s<%s Type='%s'>\033[95m%f\033[0m</%s>" % ("  " * level, key, data[key]['type'], data[key]['value'], key))
+            elif 'type' in data[key] and data[key]['type'] in ["StrProperty", "ArrayProperty", "NameProperty"]:
+                print("%s<%s Type='%s'>\033[95m%s\033[0m</%s>" % ("  " * level, key, data[key]['type'], data[key]['value'], key))
+            elif isinstance(data[key], list):
+                print("%s<%s Type='%s'>%s</%s>" % ("  " * level, key, data[key]['struct_type'] if 'struct_type' in data[
+                    key] else "\033[31munknow struct\033[0m", str(data[key]), key))
+            else:
+                print("%s<%s Type='%s'>" % ("  " * level, key, data[key]['struct_type'] if 'struct_type' in data[key] else "\033[31munknow struct\033[0m"))
+                PrettyPrint(data[key], level + 1)
+                print("%s</%s>" % ("  " * level, key))
 
 with open(player_cache,"wb") as f:
     for k in mapping:
@@ -112,62 +103,17 @@ for item in lists:
     sav = item['file']
     save_path = item['file']
     #  or int(sav[0:8], 16) not in mapping
-    if len(sys.argv) > 1 and sys.argv[1] in sav:
+    if (len(sys.argv) > 1 and sys.argv[1] in sav):
+        from lib.gvas import GvasFile
+        from lib.palsav import compress_gvas_to_sav, decompress_sav_to_gvas
+        from lib.paltypes import PALWORLD_CUSTOM_PROPERTIES, PALWORLD_TYPE_HINTS
         with open(item['file'], "rb") as f:
             # Read the file
             data = f.read()
-            uncompressed_len = int.from_bytes(data[0:4], byteorder="little")
-            compressed_len = int.from_bytes(data[4:8], byteorder="little")
-            magic_bytes = data[8:11]
-            save_type = data[11]
-            # Check for magic bytes
-            if magic_bytes != b"PlZ":
-                raise Exception(
-                    f"File {save_path} is not a save file, found {magic_bytes} instead of P1Z"
-                )
-            # Valid save types
-            if save_type not in [0x30, 0x31, 0x32]:
-                raise Exception(f"File {save_path} has an unknown save type: {save_type}")
-            # We only have 0x31 (single zlib) and 0x32 (double zlib) saves
-            if save_type not in [0x31, 0x32]:
-                raise Exception(
-                    f"File {save_path} uses an unhandled compression type: {save_type}"
-                )
-            if save_type == 0x31:
-                # Check if the compressed length is correct
-                if compressed_len != len(data) - 12:
-                    raise Exception(
-                        f"File {save_path} has an incorrect compressed length: {compressed_len}"
-                    )
-            # Decompress file
-            uncompressed_data = zlib.decompress(data[12:])
-            if save_type == 0x32:
-                # Check if the compressed length is correct
-                if compressed_len != len(uncompressed_data):
-                    raise Exception(
-                        f"File {save_path} has an incorrect compressed length: {compressed_len}"
-                    )
-                # Decompress file
-                uncompressed_data = zlib.decompress(uncompressed_data)
-            # Check if the uncompressed length is correct
-            if uncompressed_len != len(uncompressed_data):
-                raise Exception(
-                    f"File {save_path} has an incorrect uncompressed length: {uncompressed_len}"
-                )
-            # Convert to json with uesave
-            # Run uesave.exe with the uncompressed file piped as stdin
-            # stdout will be the json string
-            uesave_run = subprocess.run(
-                [uesave_path, "to-json"],
-                input=uncompressed_data,
-                capture_output=True,
-            )
-            jsondata = uesave_run.stdout.decode("utf-8")
-            # print(jsondata)
-            data = json.loads(jsondata)
-            prettySaveStruct(data['root']['properties']['SaveData'])
-            # print(data['root']['properties']['SaveData']['Struct']['value']['Struct']['PlayerUId']['Struct']['value']['Guid'])
-            # print('grep -F \'"admin_player_uid": "%s"\' -B 1 -A 10 Level.sav.json' % data['root']['properties']['SaveData']['Struct']['value']['Struct']['PlayerUId']['Struct']['value']['Guid'])
+
+            raw_gvas, _ = decompress_sav_to_gvas(data)
+            player_gvas_file = GvasFile.read(raw_gvas, PALWORLD_TYPE_HINTS, PALWORLD_CUSTOM_PROPERTIES)
+            PrettyPrint(player_gvas_file.properties['SaveData'])
             break
     elif len(sys.argv) == 1:
         print("%s -> %d\t %s\t%s\t %s" % (sav[0:8], int(sav[0:8], 16),
